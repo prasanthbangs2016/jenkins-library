02f0be72c7be75416a82348f1411593debb0d8db
Graham Hayes <graham.hayes@suse.com>
2017-07-10 14:24:00 +0100

    Turn off color output for terraform

--- vars/cleanupEnvironment.groovy
+++ vars/cleanupEnvironment.groovy
@@ -34,6 +34,7 @@ def call(Map parameters = [:]) {
                     "PREFIX=jenkins",
                     "FORCE=true",
                     "WGET_FLAGS=--no-verbose",
+                    "NO_COLOR=true",
                 ]) {
                     ansiColor('xterm') {
                         sh(script: 'set -o pipefail; ./contrib/libvirt/k8s-libvirt.sh destroy 2>&1 | tee ${WORKSPACE}/logs/terraform-destroy.log')
