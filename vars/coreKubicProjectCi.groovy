c8b5200eff30a6db1e81ea7cc7af1515223ae90b
Kiall Mac Innes <kiall@macinnes.ie>
2017-07-11 18:57:36 +0100

    Fix disallowing execution of non-collab PRs

--- vars/coreKubicProjectCi.groovy
+++ vars/coreKubicProjectCi.groovy
@@ -33,13 +33,13 @@ def call(Map parameters = [:]) {
                 echo "Test execution for collaborator ${CHANGE_AUTHOR} allowed"
 
             } else {
-                def allowExecution = input(id: 'userInput', message: "Change Author is not a Kubic Project member: ${CHANGE_AUTHOR}", parameters: [
+                def allowExecution = input(id: 'userInput', message: "Change author is not a Kubic Project member: ${CHANGE_AUTHOR}", parameters: [
                     [$class: 'BooleanParameterDefinition', defaultValue: false, description: 'Run tests anyway?', name: 'allowExecution']
                 ])
 
                 if (!allowExecution) {
                     echo "Test execution for unknown user (${CHANGE_AUTHOR}) disallowed"
-                    currentBuild.result = 'FAILURE'
+                    error(message: "Test execution for unknown user (${CHANGE_AUTHOR}) disallowed")
                     return;
                 }
             }
