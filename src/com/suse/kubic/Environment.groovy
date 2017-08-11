b1118547d9defcfa0318c0a324064fafa4a70571
Kiall Mac Innes <kiall@macinnes.ie>
2017-08-11 15:32:19 +0100

    Centralize the env JSON reading

--- src/com/suse/kubic/Environment.groovy
+++ src/com/suse/kubic/Environment.groovy
@@ -1,12 +1,38 @@
 package com.suse.kubic;
 
+import net.sf.json.JSONObject;
+
 // A Kubic Environment
 class Environment implements Serializable {
-	String dashboardHost;
-	String kubernetesHost;
+    String dashboardHost;
+    String kubernetesHost;
 
-	String sshUser;
-	String sshKey;
+    String sshUser;
+    String sshKey;
 
-	List<Minion> minions = new ArrayList<>();
+    List<Minion> minions = new ArrayList<>();
+
+    Environment() {}
+
+    Environment(JSONObject environmentJson) {
+        // Fill out the dashboardHost and kubernetesHost
+        this.dashboardHost = environmentJson.dashboardHost
+        this.kubernetesHost = environmentJson.kubernetesHost
+        this.sshUser = environmentJson.sshUser
+        this.sshKey = environmentJson.sshKey
+
+        environmentJson.minions.each { tfMinion ->
+            Minion minion = new Minion()
+
+            minion.index = tfMinion.index
+            minion.fqdn = tfMinion.fqdn
+            minion.role = tfMinion.role
+            minion.minionId = tfMinion.minionId
+            minion.proxyCommand = tfMinion.proxyCommand
+            minion.addresses.publicIpv4 = tfMinion.addresses.publicIpv4
+            minion.addresses.privateIpv4 = tfMinion.addresses.privateIpv4
+
+            this.minions.push(minion)
+        }
+    }
 }
